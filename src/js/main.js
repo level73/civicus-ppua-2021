3f871d2d10c9513e6cd35ad223bd1b18b4f7d7e9
IronRunes <code@level73.it>
2020-12-05 19:24:04 +0100

    map page

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,24 @@
 var PPUA = {
 
     init: function(){
+        this.bodyShow();
         this.headerBG();
     },
 
     headerBG: function(){
         var bg_img = $('#main-header').data('bg');
         $('#main-header').css('background-image', 'url(assets/images/' + bg_img);
+    },
+
+    bodyShow: function(){
+        $(window).on('load', function(){
+            $('body#homepage').css('display', 'block');
+        });
+
     }
 
 };
+
 
 $(document).ready(function(){
     PPUA.init();
